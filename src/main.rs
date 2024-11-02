9f523c486814421a3ba810882490d99feca3ee4e
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2024-11-02 17:47:26 +0200

    restructure & add tests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-use pest::Parser;
-use pest_derive::Parser;
-
-#[derive(Parser)]
-#[grammar = "./grammar.pest"]
-pub struct Grammar;
-
-
+///main is Ok
 fn main() -> anyhow::Result<()>{
-    let res = Grammar::parse(Rule::file, "-44.43,-6,-9\n-87.0,-5,6.7,8\n")?;
-    println!("{:?}", res);
     Ok(())
 }
 
